114aac0434c98470338dcdae464f7cef3d063403
globe45 <golusingh9714@gmail.com>
2019-04-07 22:20:17 +0530

    styling with css

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -9,14 +9,27 @@ export default class Product extends Component{
     return(
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-           <div className="img-container p-5" onclick={console.log("youu clicked me on the image container")}>
+           <div className="img-container p-5" onclick={()=>console.log("youu clicked me on the image container")}>
              <Link to="/details">
                <img src={img} alt="product" className="card-img-top" />
 
-
              </Link>
-
-
+            <button className="cart-btn" disabled={inCart?true:false} onclick={()=>{console.log("added to the cart");}}>
+            {inCart?(<p className="text-capitalize mb-0"disabled>{" "}
+             in Cart
+            </p>
+             ):(
+               <i className="fas fa-cart-plus"/>
+             )}
+            </button>
+           </div>
+           {/*card footer*/}
+           <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
            </div>
         </div>
 
@@ -28,6 +41,33 @@ export default class Product extends Component{
 
 }
 const ProductWrapper=styled.div`
+.card{
+  border-color:transparent;
+  transition:all is linear;
+}
+.card-footer{
+  background:transparent;
+  border-top:transparent;
+  transition:all is linear;
+}
+&:hover{
+  .card{
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2)
+  }
+  .card-footer{
+    background:rgba(247,247,247);
+  }
 
-
-`
+}
+.img-container{
+  position:relative;
+  overlfow:hidden;
+}
+.csrd-img-top{
+  transition:all is linear;
+}
+.img-container:hover.card-img-top{
+  transform:scale(1.2);
+}
+`;
