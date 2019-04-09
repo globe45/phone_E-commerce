badf48db058ef60f6a350c1a60dc27460aa12862
globe45 <golusingh9714@gmail.com>
2019-04-09 17:51:08 +0530

    working on detail page

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import {storeProducts,detailProduct} from "./data";
 
 const ProductContext=React.createContext();
@@ -6,15 +6,28 @@ const ProductContext=React.createContext();
 //Consumer
 class ProductProvider  extends Component {
   state={
-    products:storeProducts,
+    products:[],
     detailProduct:detailProduct
+  };
+  componentDidMount(){
+    this.setProducts();
   }
+  setProducts =() =>{
+    let tempProducts=[];
+    storeProducts.forEach(item=> {
+      const singleItem={...item};
+      tempProducts=[...tempProducts,singleItem];
+    });
+  this.setState(()=>{
+    return{products:tempProducts };
+  });
+};
   handleDetail= () =>{
     console.log("hello from detail");
-  }
+  };
   addToCart=() =>{
     console.log("hello from add to cart");
-  }
+  };
   render () {
       return(
         <ProductContext.Provider value={{
